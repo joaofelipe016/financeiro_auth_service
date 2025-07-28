3bec9c2921f321f8b441cca9affb84a1c3281c4a
joaofelipe016 <joaofelipe054@gmail.com>
2025-07-28 19:25:38 -0300

    Inicio do projeto

---
+++ src/main/java/com/project/financeiro_auth_service/FinanceiroAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.financeiro_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceiroAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceiroAuthServiceApplication.class, args);
+	}
+
+}
